271ee139a1cd00650d12a460c450cb458c406204
Faros <jessica.divers@gmail.com>
2014-09-25 18:24:18 -0700

    buy / start upgrade
    functions for buying cards, added head and upgrade arrays to stack model

--- js/script.js
+++ js/script.js
@@ -1,69 +1,83 @@
 //Library of Cards goes here
 var cardLibrary = [
-	['firstCardTitle', 4, 2, 2, 'body', '+2 power'],
-	['2', 2, 1, 2, 'body', '+2 power'], 
-	['3', 5, 2, 4, 'tail', '+4 power'],
-	['4', 3, 1, 3, 'tail', '+3 power']
+	['firstCardTitle', 4, 2, 2, 'blue', 'body', 'functionName', '+2 power'],
+	['thick thighs', 2, 1, 2, 'green', 'body', 'functionName', '+2 power'], 
+	['card number 3', 5, 2, 4, 'green', 'tail', 'functionName', '+4 power'],
+	['sticky sav', 3, 1, 3, 'green', 'tail', 'functionName', '+3 power'],
+	['sticky sav2', 3, 1, 3, 'green', 'tail', 'functionName', '+3 power'],
+	['sticky sav3', 3, 1, 3, 'green', 'tail', 'functionName', '+3 power']
 ]
 
 var starterDeck = [
-	['starterBody',0,0,1, 'body', '+1 power'],
-	['starterBody',0,0,1, 'body','+1 power'],
-	['starterBody',0,0,1, 'body','+1 power'],
-	['starterTail',0,0,2, 'tail','+2 power'],
-	['starterTail',0,0,2, 'tail','+2 power']
+	['starterBody',0,0,1, null, 'body', null, '+1 power'],
+	['starterBody',0,0,1, null, 'body', null,'+1 power'],
+	['starterBody',0,0,1, null, 'body', null,'+1 power'],
+	['starterTail',0,0,2, null, 'tail', null,'+2 power'],
+	['starterTail',0,0,2, null, 'tail', null,'+2 power']
 ]
 
 var mutations = [
-	['mutation',0,-2,0, 'tail','-2 points']
+	['mutation',0,-2,0, null, 'tail', null, '-2 points']
 ]
 
 var smallVictory = [
-	['tiny victory',2,1,0, 'body', '+1 points']
+	['tiny victory',2,1,0, null, 'body', null, '+1 points']
 ]
 
 var bigVictory = [
-	['big victory',7,5,0, 'tail', '+5 points']
+	['big victory',7,5,0, 'tail', 'tossTail', '+5 points']
 ]
 
-var smallUpgrade = [
-	//upgrades cards need color cost
+var upgrades = [
+	['upgrade01',['red','green'],2,0, 'upgrade', 'functionName', '+2 points, whatever power functionName grants'],
+	['upgrade02',['red','blue'],2,0, 'upgrade', 'functionName', '+2 points, whatever power functionName grants'],
+	['upgrade03',['blue','green'],3,0, 'upgrade', 'functionName', '+3 points, whatever power functionName grants'],
+	['upgrade04',['red','red'],3,0, 'upgrade', 'functionName', '+3 points, whatever power functionName grants'],
+	['upgrade05',['blue','blue'],4,0, 'upgrade', 'functionName', '+4 points, whatever power functionName grants'],
+	['upgrade06',['green','green'],4,0, 'upgrade', 'functionName', '+4 points, whatever power functionName grants']
+
 ]
 
-var bigUpgrade = [
-	//upgrades cards need color cost
+var heads = [
+	['head01',['red','red','green'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants'],
+	['head02',['blue','red','green'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants'],
+	['head03',['blue','blue','green'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants'],
+	['head04',['red','red','red'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants'],
+	['head05',['green','green','green'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants'],
+	['head06',['blue','blue','blue'],5,0, 'head', 'functionName', '+5 points, whatever power functionName grants']
 ]
-
-//var emptyDeck = [] don't need this
 
 function Player(playerName) {
   this.playerName = playerName;
-  this.totalScore = 0;
   this.totalPower = 0;
+  this.totalColor = [];
   this.beastSize = 0;
   this.maxbeastSize = 8;
+  this.totalScore = 0;
+  //every card player has is somewhere in deck 
   this.deck = new Stack();
   this.scoreTurn = function(){
   	//gain mutations if necessary
   	if(this.beastSize > this.maxbeastSize ){
-
+  		this.deck.discard(mutations.deal());
   	}
   	alert("You have " + this.totalPower + " and " + this.beastSize + " cards played");
   }
-  //this.stack = new Stack();
   //this.scoreTurn = function(){
   	//todo
   	//add to power score
   //}
 }
 
-function Card(cardTitle, cost, points, power, cardType, cardText) {
+function Card(cardTitle, cost, points, power, color, cardType, cardFunction, cardText) {
 	this.cardTitle = cardTitle;
 	this.cost = cost;
 	this.points = points;
 	this.power = power;
 	this.cardType = cardType;
 	this.cardText = cardText;
+	this.cardColor = color;
+	this.cardFunction = cardFunction;
 	//this.toString = function(card){};
 }
 
@@ -72,8 +86,8 @@ function Stack(){
 	this.hand;
 	this.discardPile;
 	this.played;
-	this.other; //extra array for upgrade cards etc
-	this.head; //TODO implement head scoring
+	this.upgrades;
+	this.heads; 
 	this.createStack = function(cardArray){
 		var cards = [];
 		for (var i=0; i < cardArray.length; i++){
@@ -84,7 +98,8 @@ function Stack(){
 	this.hand = [];
 	this.discardPile = [];
 	this.played = [];
-	this.other = [];
+	this.upgrades = [];
+	this.heads = [];
 	}
 	//used for cardMultiplier function, not gameplay
 	this.addCard = function(card){
@@ -109,7 +124,6 @@ function Stack(){
 			return this.cards.shift();
 		} else {
 			return null;
-			//shuffle deck and then draw?
 		}
 	}
 	//add cards to different stack arrays
@@ -120,14 +134,27 @@ function Stack(){
 		this.discardPile.push(card);
 	}
 	this.playCard = function(card, player){
+		//play card
 		this.played.push(card);
+
+		//score card power 
 		player.totalPower += card.power;
+
+		//score card color
+		if (card.cardColor != null){
+			player.totalColor.push(card.cardColor);
+		}
+
+		//increment beast size
 		player.beastSize ++;
 
 		//end turn if tail
 		if (card.cardType == 'tail'){
 			window.noTail = 'tail';
 		}
+
+		//log
+		$('#console').append('<p>Played '+card.cardTitle+" with power: "+card.power+"and color: "+card.cardColor+".</p>");
 	}
 	//get info 
 	this.checkDiscardPile = function(){
@@ -141,8 +168,10 @@ function cardMultiplier(stack, card, num){
 	}
 }
 
-function buyCards(){
-
+//Card functions
+//example
+function tossTail(){
+	//prompts user to keep or discard tail, then evalutes card as normal if kept
 }
 
 
